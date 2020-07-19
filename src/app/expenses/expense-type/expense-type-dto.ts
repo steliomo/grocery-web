3932f0a2c60198f7d323bc4dd8e6000de5b07bd8
= <=>
2020-07-20 00:36:01 +0200

    GRW-04: Adding Lazy loading and ExpenseTypeCategory field

--- src/app/expenses/expense-type/expense-type-dto.ts
+++ src/app/expenses/expense-type/expense-type-dto.ts
@@ -1,6 +1,8 @@
 import { GenericDTO } from '../../core/dto/generic-dto';
+import { ExpenseTypeCategory } from './expense-type-category';
 
 export interface ExpenseTypeDTO extends GenericDTO {
+    expenseTypeCategory: ExpenseTypeCategory,
     name: string,
     description: string;
 }
